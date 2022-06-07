5354e8f4d5a33185b40debdb7d93ab40cbadac45
JuniorLopezSilvestre <junior.lopez.silvestre@gmail.com>
2022-06-07 00:50:27 -0500

    item count

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 import MUNavbar from './components/MUNavbar';
 import Background from './components/Bg';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
+
+
+
 function App() {
+
+const onAdd = (count) =>{
+  alert(`Agregaste ${count} productos` );
+}
+
+
   return (
     <><MUNavbar/>
-    <ItemListContainer Lista={'Aqui van los articulos'}/>
+    <ItemListContainer Lista={'Bienvenidos'}/>
+    <ItemCount inicial={1} max={10} onAdd={onAdd}/>
     <Background/>
 </>
   );
