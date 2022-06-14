57496f55aa2d4ea3eaef0fd6d2f9d375e3a2bfc3
JuniorLopezSilvestre <junior.lopez.silvestre@gmail.com>
2022-06-14 00:54:01 -0500

    fixes desafío

--- src/components/views/Item.jsx
+++ src/components/views/Item.jsx
@@ -1,27 +1,33 @@
 import React, { useState } from 'react'
+import Grid from '@mui/material/Grid';
 import ItemList from '../views/ItemList';
 import imgProduct1 from '../../assets/images/products/product1.jpg'
 import imgProduct2 from '../../assets/images/products/product2.jpg'
 import imgProduct3 from '../../assets/images/products/product3.jpg'
 import imgProduct4 from '../../assets/images/products/product4.jpg'
+import imgProduct5 from '../../assets/images/products/product5.jpg'
+import imgProduct6 from '../../assets/images/products/product6.jpg'
+import imgProduct7 from '../../assets/images/products/product7.jpg'
 
 const items = [
-  {title:'Polo', id:'0', price:'100$', image:`${imgProduct1}`},
-  {title:'Polo', id:'1', price:'100$', image:`${imgProduct2}`},
-  {title:'Polo', id:'2', price:'100$', image:`${imgProduct3}`},
-  {title:'Polo', id:'3', price:'100$', image:`${imgProduct4}`},
-  {title:'Polo', id:'4', price:'100$', image:`${imgProduct1}`},
-  {title:'Polo', id:'5', price:'100$', image:`${imgProduct1}`},
-  {title:'Polo', id:'6', price:'100$', image:`${imgProduct1}`},
+  {title:'McCormick® Original Taco Seasoning Mix', id:'0', price:'$2.29', image:`${imgProduct1}`},
+  {title:'Signature Kitchens Honey, Clover', id:'1', price:'$5.79', image:`${imgProduct2}`},
+  {title:'Green Onions (Scallions)', id:'2', price:'$1.79', image:`${imgProduct3}`},
+  {title:'Driscolls Organic Raspberries', id:'3', price:'$6.89', image:`${imgProduct4}`},
+  {title:'Tree Top Apple Sauce, No Sugar Added, Pouches', id:'4', price:'$9.19', image:`${imgProduct5}`},
+  {title:'FF Anti Biotic Free Simply Raised Skinless Boneless Chicken Thigh', id:'5', price:'$10.39', image:`${imgProduct6}`},
+  {title:'Dove Body Wash Deep Moisture', id:'6', price:'$10.99', image:`${imgProduct7}`},
 ];
 
 export default function Item() {
   return (
-    <div>
+    <Grid container spacing={3} maxWidth="1300px" margin="auto">
       {items.map((item)=>
-        (<ItemList title={item.title} id={item.id} price={item.price} image={item.image}/> )
+        (
+          <ItemList title={item.title} id={item.id} price={item.price} image={item.image}/>
+        )
       )}
-    </div>
+    </Grid>
   )
 }
 
--- src/components/views/ItemList.jsx
+++ src/components/views/ItemList.jsx
@@ -1,44 +1,46 @@
 import * as React from 'react';
-import { Card, CardHeader, CardMedia, CardContent, CardActions, IconButton, Button, Typography, Box}  from '@mui/material';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, IconButton, Button, Typography, Box, Grid}  from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import IconPlus from '@mui/icons-material/Add'
 
     const ItemList = (props) => {
     const {title, id, price, image}= props;
     return (
-      <Card sx={{ maxWidth: 345, my: 2 }} elevation={0}>
-        <Box className="cardBox">
-          <CardMedia
-            component="img"
-            height="150"
-            image={image}
-            alt={image}
+      <Grid item xs={6} lg={3}>
+        <Card sx={{ my: 2 }} elevation={0}>
+          <Box className="cardBox">
+            <CardMedia
+              component="img"
+              height="150"
+              image={image}
+              alt={image}
+            />
+            <Button variant="contained" startIcon={<IconPlus/>}>Add</Button>
+          </Box>
+          <CardHeader
+            title={title}
+            subheader={
+              <Typography variant="body2" color="primary">
+                Organic 
+              </Typography>
+            }
           />
-          <Button variant="contained" startIcon={<IconPlus/>}>Add</Button>
-        </Box>
-        <CardHeader
-          title={title}
-          subheader={
-            <Typography variant="body2" color="primary">
-              Organic 
+          <CardContent>
+            <Typography variant="h6" color="text.primary">
+              {price}
             </Typography>
-          }
-        />
-        <CardContent>
-          <Typography variant="h6" color="text.primary">
-            {price}
-          </Typography>
-          <Typography sx={{display:"none"}}>
-             {id}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites" color="error">
-            <FavoriteIcon />
-          </IconButton>
-          <Button size="small" variant="text">View detail</Button>
-        </CardActions>
-      </Card>
+            <Typography sx={{display:"none"}}>
+              {id}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites" color="error">
+              <FavoriteIcon />
+            </IconButton>
+            <Button size="small" variant="text">View detail</Button>
+          </CardActions>
+        </Card>
+      </Grid>
     );
   }
 export default ItemList
