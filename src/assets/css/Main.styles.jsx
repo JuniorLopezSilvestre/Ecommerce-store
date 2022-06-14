9971953a17cbfbd2abaa28c3a8fd787c449986f1
JuniorLopezSilvestre <junior.lopez.silvestre@gmail.com>
2022-06-13 23:46:39 -0500

    array and map instacart

--- src/assets/css/Main.styles.jsx
+++
@@ -1,6 +0,0 @@
-import { flexbox, SxProps } from "@mui/system";
-import { Theme } from "@mui/material";
-
-interface IStyles {
-    [k: string]: SxProps<Theme>;
-  }
--- src/components/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Logo from '../assets/logo.svg'
-export default function logo() {
-  return (
-    <img src={Logo} alt="Logex" style={{width:'92px'}}/>
-  )
-}
